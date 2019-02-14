f3c45b04c71aa9d8fd149a6de88062e5e8873d9f
herlianali <aliyasa@umsida.ac.id>
2019-02-14 13:36:32 +0700

    tabel

--- admin/susunan-keluarga.php
+++ admin/susunan-keluarga.php
@@ -17,23 +17,45 @@
             <th>NAMA</th>
             <th>KECAMATAN</th>
             <th>DESA</th>
+            <th>Aksi</th>
+
           </tr>
         </thead>
         <tbody>
           <?php
-          if(isset($_GET['rowid'])){
+          // if(isset($_GET['rowid'])){
+          //   include "../koneksi.php";
+          //   $id = $_GET['rowid'];
+          //   $no = 1;
+          //   //echo "select * from penduduk where nkk='$id' order by tgl_lahir asc";
+          //   $data = pg_query($dpt,"select * from penduduk1 where nkk='$id' order by tgl_lahir asc");
+          //   while($row = pg_fetch_array($data)){
+          //     echo "<tr><td>".$no++."</td>
+          //     <td>".$row['nik']."</td>
+          //     <td>".$row['nama']."</td>
+          //     <td>".$row['kecamatan']."</td>
+          //     <td>".$row['desa']."</td>
+          //     <td>"<button>Hapus</button>"</td>
+          //     </tr>";
+          //   }
+          // }
+          ?>
+          <?php
+          if (isset($_GET['rowid'])) {
             include "../koneksi.php";
             $id = $_GET['rowid'];
             $no = 1;
-            //echo "select * from penduduk where nkk='$id' order by tgl_lahir asc";
             $data = pg_query($dpt,"select * from penduduk1 where nkk='$id' order by tgl_lahir asc");
-            while($row = pg_fetch_array($data)){
-              echo "<tr><td>".$no++."</td>
-              <td>".$row['nik']."</td>
-              <td>".$row['nama']."</td>
-              <td>".$row['kecamatan']."</td>
-              <td>".$row['desa']."</td>
-              </tr>";
+            while ($row = pg_fetch_array($data)) {
+            ?><tr>
+            <td><?php echo $no++; ?></td>
+            <td><?php echo $row['nik']; ?></td>
+            <td><?php echo $row['nama']; ?></td>
+            <td><?php echo $row['kecamatan']; ?></td>
+            <td><?php echo $row['desa']; ?></td>
+            <td><a type="button" href="hapus_susunan_keluarga.php?id=<?php echo $row['id'] ?>" class="btn btn-primary">hapus</a></td>
+            </tr>
+            <?php
             }
           }
           ?>
